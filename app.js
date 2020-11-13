5e4787b70704b4e57c037d6c31a78a621a5fa796
Arnold <antaloczi.arnold@gmail.com>
2020-11-13 17:20:39 +0100

    Added click event listener

---
+++ app.js
@@ -0,0 +1,6 @@
+const showModalBtn = document.querySelector(".show-modal-btn");
+const modal = document.querySelector(".modal");
+
+showModalBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
